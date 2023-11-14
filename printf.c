514c61dc1ba699fde51133ce26dbd2244c4d68fc
AdnanRim <adnanrime@gmail.com>
2023-11-14 11:38:06 -0800

    main

--- printf.c
+++ printf.c
@@ -1,6 +1,22 @@
 #include <stdarg.h>
 #include "main.h"
 #include <unistd.h>
+/**
+ * printf.c - Implementation of basic printf functionality
+ * This file contains an implementation of the printf() function with
+ * support for basic formatting specifiers (%d, %i, %c, %s).
+ * The functions in this file mimic some of the core functionality of
+ * the standard printf() function from the C library. It implements
+ * variable argument parsing and formatting/output of common integer
+ * and string types.
+ * Limitations:
+ * Only supports %d, %i, %c, %s specifiers
+ * No padding, alignment or field width handling
+ * No support for floating point or other complex types
+ * Buffer size for output is fixed
+ * Dependencies:
+ * stdio.h (for printf prototype and FILE type)
+ */
 int _printc(char c) {
 write(1, &c, 1);
 return (1);
