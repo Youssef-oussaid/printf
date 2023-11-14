0bdcf385f8aaabc11acfad06dad870145e09effe
AdnanRim <adnanrime@gmail.com>
2023-11-14 14:25:17 -0800

    printf

--- printf.c
+++ printf.c
@@ -18,17 +18,13 @@
  * Dependencies:
  * stdio.h (for printf prototype and FILE type)
  */
-int _printc(char c) {
-write(1, &c, 1);
-return (1);
-}
 int _prints(char *s) {
 int len = 0;
 while (*s) {
 len += write(1, s, 1);
 s++;
 }
-return (len);
+return len;
 }
 int _printf(const char *format, ...)
 {
@@ -36,60 +32,31 @@ int len = 0;
 va_list args;
 va_start(args, format);
 while (*format) {
-char buffer[1024];
-int count;
 if (*format == '%') {
 format++;
 switch (*format) {
-case 'c': {
-char c = va_arg(args, int);
-len += write(1, &c, 1);
-break;
-}
-case 's': {
-char *s = va_arg(args, char *);
-len += _prints(s);
-break;
-}
 case 'd':
 case 'i': {
 int num = va_arg(args, int);
+char buffer[32];
+int count;
 count = snprintf(buffer, sizeof(buffer), "%d", num);
 len += write(1, buffer, count);
-break;
-}
-case 'u': {
-unsigned int num = va_arg(args, unsigned int);
-count = snprintf(buffer, sizeof(buffer), "%u", num);
-len += write(1, buffer, count);
-break;
-}
-case 'o': {
-unsigned int num = va_arg(args, unsigned int);
-count = snprintf(buffer, sizeof(buffer), "%o", num);
-len += write(1, buffer, count);
-break;
-}
-case 'x':
-case 'X': {
-unsigned int num = va_arg(args, unsigned int);
-count = snprintf(buffer, sizeof(buffer), "%x", num);
-len += write(1, buffer, count);
-break;
-}
-case '%': {
-len += write(1, "%", 1);
 break;
 }
 default:
 len += write(1, format, 1);
 break;
 }
-} else {
+}
+else {
 len += write(1, format, 1);
 }
 format++;
 }
 va_end(args);
 return (len);
+}
+int _printc(char c) {
+return write(1, &c, 1);
 }
